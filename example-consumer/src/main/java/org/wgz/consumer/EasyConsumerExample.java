8e9b8fcc67c336bdbad1e682054b4bb96f9bb57b
Wgz <1808365012@mail.ynu.edu.cn>
2024-03-23 17:33:34 +0800

    基本RPC框架

--- example-consumer/src/main/java/org/wgz/consumer/EasyConsumerExample.java
+++ example-consumer/src/main/java/org/wgz/consumer/EasyConsumerExample.java
@@ -2,11 +2,12 @@ package org.wgz.consumer;
 
 import org.wgz.common.model.User;
 import org.wgz.common.service.UserService;
+import org.wgz.proxy.ServiceProxyFactory;
 
 public class EasyConsumerExample {
     public static void main(String[] args) {
         //todo 获取UserService的实现类
-        UserService userService = null;
+        UserService userService = ServiceProxyFactory.getProxy(UserService.class);
         User user = new User();
         user.setName("hello world");
         User newUser = userService.getUser(user);
---
+++ rpc-easy/src/main/java/org/wgz/proxy/ServiceProxy.java
@@ -0,0 +1,69 @@
+package org.wgz.proxy;
+
+import cn.hutool.http.HttpRequest;
+import cn.hutool.http.HttpResponse;
+import org.wgz.model.RpcRequest;
+import org.wgz.model.RpcResponse;
+import org.wgz.serializer.JdkSerializer;
+import org.wgz.serializer.Serializer;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+
+/**
+ * 服务代理
+ */
+public class ServiceProxy implements InvocationHandler {
+
+    /**
+     * 调用代理
+     *
+     * @param proxy  the proxy instance that the method was invoked on
+     * @param method the {@code Method} instance corresponding to
+     *               the interface method invoked on the proxy instance.  The declaring
+     *               class of the {@code Method} object will be the interface that
+     *               the method was declared in, which may be a superinterface of the
+     *               proxy interface that the proxy class inherits the method through.
+     * @param args   an array of objects containing the values of the
+     *               arguments passed in the method invocation on the proxy instance,
+     *               or {@code null} if interface method takes no arguments.
+     *               Arguments of primitive types are wrapped in instances of the
+     *               appropriate primitive wrapper class, such as
+     *               {@code java.lang.Integer} or {@code java.lang.Boolean}.
+     * @return
+     * @throws Throwable
+     */
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+
+        //指定序列化器
+        Serializer serializer = new JdkSerializer();
+
+        //构造请求
+        RpcRequest rpcRequest = RpcRequest.builder()
+                .serviceName(method.getDeclaringClass().getName())
+                .methodName(method.getName())
+                .parameterTypes(method.getParameterTypes())
+                .args(args).build();
+
+        try {
+            //序列化
+            byte[] bytes = serializer.serialize(rpcRequest);
+            byte[] result;
+
+            //发起请求
+            try (HttpResponse httpResponse = HttpRequest.post("htttp://localhost:8080")
+                    .body(bytes)
+                    .execute()) {
+                result = httpResponse.bodyBytes();
+
+                //反序列化
+                RpcResponse rpcResponse = serializer.deserialize(result, RpcResponse.class);
+                return rpcResponse.getData();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
